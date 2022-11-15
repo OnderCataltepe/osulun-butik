4b45dcbf41df18b47349e21c936f8831a2a1749f
OnderCataltepe <ondercataltepe@gmail.com>
2022-11-15 21:37:43 +0300

    Edit layout as a root element of routers

---
+++ src/layouts/Layout.tsx
@@ -0,0 +1,23 @@
+import Navbar from '../components/navbar/Navbar';
+import Footer from '../components/footer/Footer';
+import { Outlet } from 'react-router-dom';
+import { ScrollRestoration } from 'react-router-dom';
+
+const Layout = () => {
+  return (
+    <div
+      style={{
+        minHeight: '100vh',
+        maxWidth: '100%',
+        display: 'flex',
+        flexDirection: 'column'
+      }}>
+      <Navbar />
+      <Outlet />
+      <Footer />
+      <ScrollRestoration />
+    </div>
+  );
+};
+
+export default Layout;
