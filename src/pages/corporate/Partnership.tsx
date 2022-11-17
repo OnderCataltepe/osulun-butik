72f45688e991fec014719288e8fc9159a6311bf3
OnderCataltepe <ondercataltepe@gmail.com>
2022-11-17 23:28:44 +0300

    Add content

--- src/pages/corporate/Partnership.tsx
+++ src/pages/corporate/Partnership.tsx
@@ -3,196 +3,80 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import { useTheme } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
-import grayBg from '../../assets/backgrounds/grayBg.jpg';
-import simon from '../../assets/partners/simon.jpg';
-
-const partners = [{ name: 'Simon Leviev', description: 'lorem', backImg: grayBg, img: simon }];
-
+import { partners } from './constants';
 const Partnerships = () => {
   const theme = useTheme();
   const isMobil = useMediaQuery(theme.breakpoints.down('md'));
   return (
-    <Box sx={{ width: '100%', display: 'flex', flexDirection: 'column', alignItems: ' center' }}>
-      <Animate.FadeIn>
-        <h1>Hello World</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio sequi libero ut, tenetur
-          fugit rerum distinctio non recusandae expedita ipsum!
-        </p>
-      </Animate.FadeIn>
-      <Animate.FadeUp>
-        <h1>Hello World</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio sequi libero ut, tenetur
-          fugit rerum distinctio non recusandae expedita ipsum!
-        </p>
-      </Animate.FadeUp>
-      <Animate.ScaleIn>
-        <h1>Hello World</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio sequi libero ut, tenetur
-          fugit rerum distinctio non recusandae expedita ipsum!
-        </p>
-      </Animate.ScaleIn>
-      <Animate.FadeIn>
-        <h1>Hello World</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio sequi libero ut, tenetur
-          fugit rerum distinctio non recusandae expedita ipsum!
-        </p>
-      </Animate.FadeIn>
-      <Animate.FadeUp>
-        <h1>Hello World</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio sequi libero ut, tenetur
-          fugit rerum distinctio non recusandae expedita ipsum!
-        </p>
-      </Animate.FadeUp>
-      <Animate.ScaleIn>
-        <h1>Hello World</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio sequi libero ut, tenetur
-          fugit rerum distinctio non recusandae expedita ipsum!
-        </p>
-      </Animate.ScaleIn>
-      <Animate.FadeIn>
-        <h1>Hello World</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio sequi libero ut, tenetur
-          fugit rerum distinctio non recusandae expedita ipsum!
-        </p>
-      </Animate.FadeIn>
-      <Animate.FadeUp>
-        <h1>Hello World</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio sequi libero ut, tenetur
-          fugit rerum distinctio non recusandae expedita ipsum!
-        </p>
-      </Animate.FadeUp>
-      <Animate.ScaleIn>
-        <h1>Hello World</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio sequi libero ut, tenetur
-          fugit rerum distinctio non recusandae expedita ipsum!
-        </p>
-      </Animate.ScaleIn>
-      <Animate.FadeIn>
-        <h1>Hello World</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio sequi libero ut, tenetur
-          fugit rerum distinctio non recusandae expedita ipsum!
-        </p>
-      </Animate.FadeIn>
-      <Animate.FadeUp>
-        <h1>Hello World</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio sequi libero ut, tenetur
-          fugit rerum distinctio non recusandae expedita ipsum!
-        </p>
-      </Animate.FadeUp>
-      <Animate.ScaleIn>
-        <h1>Hello World</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio sequi libero ut, tenetur
-          fugit rerum distinctio non recusandae expedita ipsum!
-        </p>
-      </Animate.ScaleIn>
-      <Animate.FadeIn>
-        <h1>Hello World</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio sequi libero ut, tenetur
-          fugit rerum distinctio non recusandae expedita ipsum!
-        </p>
-      </Animate.FadeIn>
-      <Animate.FadeUp>
-        <h1>Hello World</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio sequi libero ut, tenetur
-          fugit rerum distinctio non recusandae expedita ipsum!
-        </p>
-      </Animate.FadeUp>
-      <Animate.ScaleIn>
-        <h1>Hello World</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio sequi libero ut, tenetur
-          fugit rerum distinctio non recusandae expedita ipsum!
-        </p>
-      </Animate.ScaleIn>
-      <Animate.FadeLeft>
-        <h1>Hello World</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio sequi libero ut, tenetur
-          fugit rerum distinctio non recusandae expedita ipsum!
-        </p>
-      </Animate.FadeLeft>
-      <Animate.FadeUp>
-        <h1>Hello World</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio sequi libero ut, tenetur
-          fugit rerum distinctio non recusandae expedita ipsum!
-        </p>
-      </Animate.FadeUp>
-      <Animate.ScaleIn>
-        <h1>Hello World</h1>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Optio sequi libero ut, tenetur
-          fugit rerum distinctio non recusandae expedita ipsum!
-        </p>
-      </Animate.ScaleIn>
-      <Box
-        sx={{
-          width: '100%',
-          height: 'auto',
-          p: 4,
-          backgroundSize: 'cover',
-          backgroundRepeat: 'no-repeat',
-          backgroundImage: `url(${grayBg})`,
-          textAlign: 'center'
-        }}>
-        <Animate.ScaleIn>
-          <Typography variant="h2">Simon Leviev</Typography>
-        </Animate.ScaleIn>
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: isMobil ? 'column' : 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            width: '100%',
-            '& >div': { width: isMobil ? '80%' : '40%', height: 'auto' }
-          }}>
-          <Animate.FadeRight>
-            <div style={{ width: '95%', boxShadow: '0px 0px 40px -20px #fff' }}>
-              <img
-                src={simon}
-                style={{
-                  width: '100%'
-                }}
-              />
-            </div>
-          </Animate.FadeRight>
-          <Animate.FadeLeft>
-            <Typography variant="body1" sx={{ color: 'white' }}>
-              {' '}
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa reprehenderit, quidem
-              voluptate quos veritatis maxime optio animi accusantium veniam ab ex incidunt beatae
-              dolor tenetur itaque eius libero reiciendis porro. Corrupti accusamus sint enim?
-              Magnam eaque iusto maxime distinctio. Perferendis excepturi, officiis ut possimus in
-              natus voluptatum eveniet nobis vel doloremque blanditiis, impedit consequuntur ipsa
-              assumenda. Nulla, cumque quae. Recusandae? Nostrum laudantium, repellendus saepe
-              explicabo, corporis quasi alias quas quibusdam aspernatur, amet quisquam omnis minima
-              fugit magni natus iure repellat provident molestias eaque facere sint corrupti nulla
-              architecto! Reiciendis, aspernatur! Optio laudantium maiores dignissimos alias eos
-              atque molestias, dicta vitae temporibus inventore suscipit qui nesciunt deserunt
-              recusandae repudiandae quis tempore. Soluta doloremque obcaecati nihil quidem
-              architecto aperiam non vitae nisi! Odit, esse rerum explicabo voluptatum voluptates
-              error reiciendis sed ea ex itaque molestias ut eos distinctio autem obcaecati
-              repellendus deserunt, dolorum officia, nemo amet. Rem quo perferendis voluptatem sed
-              dignissimos?{' '}
-            </Typography>
-          </Animate.FadeLeft>
-        </Box>
+    <Box
+      sx={{
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: ' center',
+        overflow: 'hidden'
+      }}>
+      <Box sx={{ textAlign: 'center', my: 6 }}>
+        <Typography variant="h1">İş Ortaklarımız</Typography>
+        <Typography
+          variant="body1"
+          sx={{ color: theme.palette.common.brown1, py: 4, px: isMobil ? 6 : 20 }}>
+          Sınırlı sayıda tuttuğumuz iş ortaklarımızın tüm ürünlerine yoğun yatırımlar yaparak bilgi
+          ve tecrübemizi maksimum düzeyde tutmaya çalışıyoruz. Müşterilerimize sunduğumuz ve
+          desteğini vermeye devam ettiğimiz teknolojilerde tüm personelimizin tecrübesini arttırarak
+          hizmet kalitemizi ve müşteri memnuniyetini arttırıyoruz.
+        </Typography>
       </Box>
+      {partners.map((item, index) => {
+        return (
+          <Box
+            key={index}
+            sx={{
+              width: '100%',
+              height: 'auto',
+              p: 4,
+              backgroundColor: item.bgColor,
+              textAlign: 'center'
+            }}>
+            <Animate.ScaleIn>
+              <Typography variant="h2">{item.name}</Typography>
+            </Animate.ScaleIn>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: isMobil ? 'column' : index % 2 === 0 ? 'row' : 'row-reverse',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                width: '100%',
+                '& >div': { width: isMobil ? '80%' : '45%', height: 'auto' }
+              }}>
+              <Animate.FadeRight>
+                <Box
+                  sx={{
+                    width: '75%',
+                    boxShadow: '0px 0px 30px -20px #fff',
+                    borderRadius: '50%',
+                    m: '30px auto',
+                    overflow: 'hidden'
+                  }}>
+                  <img
+                    src={item.img}
+                    style={{
+                      width: '100%'
+                    }}
+                  />
+                </Box>
+              </Animate.FadeRight>
+              <Animate.FadeLeft>
+                <Typography variant="body1" sx={{ color: 'white', fontWeight: 'bold' }}>
+                  {item.description}
+                  {item.italic.length > 0 && <i style={{ fontWeight: 'lighter' }}>{item.italic}</i>}
+                </Typography>
+              </Animate.FadeLeft>
+            </Box>
+          </Box>
+        );
+      })}
     </Box>
   );
 };
